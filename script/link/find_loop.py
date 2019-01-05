33356904cc7d691a2c9f3b7353d058cbdfe2eed6
hongliang5623 <zhanghliang5623@126.com>
2019-01-05 04:52:25 -0800

    find loop

---
+++ script/link/find_loop.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+class Node(): #定义一个Node类，构造两个属性，一个是item节点值，一个是节点的下一个指向
+    def __init__(self,item=None):
+         self.item = item
+         self.next = None
+
+
+def findbeginofloop(head):#判断是否为环结构并且查找环结构的入口节点
+     slowPtr = head         #将头节点赋予slowPtr
+     fastPtr = head         #将头节点赋予fastPtr
+     loopExist =False       #默认环不存在，为False
+     if head == None:       #如果头节点就是空的，那肯定就不存在环结构
+         return False
+     while fastPtr.next != None and fastPtr.next.next != None:      #fastPtr的下一个节点和下下个节点都不为空
+         slowPtr = slowPtr.next           #slowPtr每次移动一个节点
+         fastPtr = fastPtr.next.next      #fastPtr每次移动两个节点
+         if slowPtr == fastPtr :          #当fastPtr和slowPtr的节点相同时，也就是两个指针相遇了
+             loopExist = True
+             print("存在环结构")
+             break
+
+     print 'fats local:', fastPtr.item
+     if loopExist == True:
+         slowPtr  = head
+         while slowPtr != fastPtr:
+             print 'slow:%s, fast:%s' % (slowPtr.item, fastPtr.item)
+             fastPtr = fastPtr.next
+             slowPtr = slowPtr.next
+         return slowPtr
+
+     print("不是环结构")
+     return False
+
+
+if __name__ == '__main__':
+    node1 = Node(1)
+    node11 = Node(11)
+    node12 = Node(12)
+    node2 = Node(2)
+    node3 = Node(3)
+    node4 = Node(4)
+    node5 = Node(5)
+    node1.next = node11
+    node11.next = node12
+    node12.next = node2
+    node2.next = node3
+    node3.next = node4
+    node4.next = node5
+    node5.next = node2
+    print(findbeginofloop(node1).item)
